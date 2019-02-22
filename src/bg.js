7db9aec837635bb044e391c67a067279e43ea9ef
Drip <contact@s4wa.me>
2019-02-22 15:36:24 +0900

    #2

--- src/bg.js
+++ src/bg.js
@@ -4,18 +4,19 @@ chrome.contextMenus.create({
 	"onclick": function () {
 		chrome.tabs.getSelected(null, function(tab) {
 			var str = tab.title + " " + tab.url;
-			console.log(str);
+			// console.log(str);
 
 			chrome.tabs.query({
 				url: "*://misskey.xyz/*"
 			}, function (result) {
 				if (result.length !== 0) {
 					chrome.tabs.update(result[0].id, { active : true }, function(tab) {
-						var cmd = "var drafts = JSON.parse(localStorage.drafts); drafts.note.data.text = \"" + str + "\"; localStorage.drafts = JSON.stringify(drafts);";
-						cmd += "var arena = document.getElementsByClassName(\"textarea\"), elem = arena[arena.length-1];" + 
+						var cmd = "var drafts = JSON.parse(localStorage.drafts); drafts.note.data.text = \"" + str + "\"; localStorage.drafts = JSON.stringify(drafts); location.reload();";
+						/*cmd += "var arena = document.getElementsByClassName(\"textarea\"), elem = arena[0];" + 
 							"if (arena.length !== 0 && elem.hasChildNodes) {elem.childNodes[0].innerText = \"" + str + "\";}" + 
-							"if (elem.hasChildNodes) {elem.childNodes[elem.childNodes.length-1].disabled = \"enabled\";}";
+							"var btn = document.getElementsByClassName(\"dmtdnykelhudezerjlfpbhgovrgnqqgr submit primary\"); if (btn.length !== 0) {for (var i = 0; i < btn.length; i++) {btn[i].disabled = false;}} ";*/
 
+						console.log(cmd);
 						chrome.tabs.executeScript(tab.id, {
 								code: cmd
 							}
