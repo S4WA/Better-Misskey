06cc68b51ccb7044a27633b0493e9d71047f3b32
Drip <contact@s4wa.me>
2019-02-25 05:57:30 +0900

    Add files via upload
    #1

--- src/options/options.js
+++ src/options/options.js
@@ -1,22 +1,42 @@
-var saveBtn = document.body, map = [
-	{"move-columns": true},
-	{"draw-outline": true},
-	{"draw-numbers": true},
-	{"close-image": true},
-	{"resize-image": true},
-	{"share": true}
-];
+var map = {
+		"move_columns": true,
+		"draw_outline": true,
+		"draw_numbers": true,
+		"close_image": true,
+		"resize_image": true,
+		"share": true
+	}, data = {}, saveBtn, box;
 
 window.onload = function () {
-	saveBtn = document.getElementById("save");
-	// saveBtn.disabled = true;
+	load();
 
-	saveBtn.onclick = function() {
-		save();
-	};
+	saveBtn = document.getElementById("save");
+	box = document.getElementsByClassName("box");
+
+	for (var i = 0; i < box.length; i++) box[i].checked = data[box[i].id];
+
+	saveBtn.onclick = function() { save(); };
 };
 
 function save() {
+	var box = document.getElementsByClassName("box");
+	for (var i = 0; i < box.length; i++) map[box[i].id] = box[i].checked;
+
+	chrome.storage.sync.set(map, function() {
+		var doc = document.getElementById("status");
+		if (doc.innerText !== "") {
+			doc.innerText = "saved!";
+			setTimeout(function() {
+				doc.innerText = "";
+			}, 900);
+		}
+	});
+}
+
+function load() {
+	chrome.storage.sync.get(null, function(items) {
+		for (var key in items) data[key] = items[key];
+	});
 }
 
 /*for (var i = 0; i < map.length; i++) {
@@ -30,3 +50,8 @@ function save() {
 for (var i = 0; i < map.length; i++) {
 	if (JSON.stringify(map[i]) === JSON.stringify(data[i])) isEqual = false;
 }*/
+
+/*
+map = default settings.
+data = user's settings.
+*/
