6e2431bc3a4892d3301f388d0bbc844b8f747caf
Drip <contact@s4wa.me>
2019-03-03 10:12:49 +0900

    Add files via upload

--- src/options/options.js
+++ src/options/options.js
@@ -5,8 +5,37 @@ var map = {
 	"close_image": true,
 	"resize_image": true,
 	"share": true,
-	"reload_all" : true
-}, data = {}, saveBtn, box;
+	"reload_all" : true,
+	"language": "japanese"
+},
+data = {},
+language = {
+	"japanese": {
+		"move_columns": "カラムを数字キーで移動する",
+		"draw_outline": "カラムを数字キーで選んだ時にアウトラインを表示する",
+		"draw_numbers": "カラムの数字を表示する",
+		"close_image": "エスケープキーで画像を閉じる",
+		"resize_image": "画像をリサイズする",
+		"share": "タブの共有",
+		"reload_all" : "設定を保存したとき全Misskeyのタブをリロードする",
+		"save": "保存",
+		"better-misskey-settings": "Better Misskey 設定",
+		"settings": "設定"
+	},
+	"english": {
+		"move_columns": "Move column with number key",
+		"draw_outline": "On select column with number key to draw outline",
+		"draw_numbers": "Draw number to columns",
+		"close_image": "On press escape key to close image",
+		"resize_image": "Resize image",
+		"share": "Share tab",
+		"reload_all" : "On save settings to reload all misskey tabs",
+		"save": "Save",
+		"better-misskey-settings": "Better Misskey Options",
+		"settings": "Settings"
+	}
+},
+saveBtn, box;
 
 window.onload = function() {
 	load();
@@ -16,16 +45,19 @@ window.onload = function() {
 };
 
 function save() {
-	var box = document.getElementsByClassName("box");
+	var box = document.getElementsByClassName("box"), saveBtn = document.getElementById("save");
 	for (var i = 0; i < box.length; i++) map[box[i].id] = box[i].checked;
+	map["language"] = document.getElementById("language").value;
 
 	chrome.storage.sync.set(map, function() {
 		var doc = document.getElementById("status");
-		if (doc.innerText !== "") {
+		if (doc.innerText === "") {
+			saveBtn.disabled = true;
 			doc.innerText = "saved!";
 			setTimeout(function() {
+				saveBtn.disabled = false;
 				doc.innerText = "";
-			}, 900);
+			}, 500);
 		}
 	});
 
@@ -38,6 +70,9 @@ function load() {
 			data[key] = items[key];
 			if (document.getElementById(key) !== null) document.getElementById(key).checked = items[key];
 		}
+		var lang = items["language"].toLowerCase();
+		document.getElementById(lang).selected = true;
+		putLangs(lang);
 	});
 }
 
@@ -45,13 +80,24 @@ function reloadAll() {
 	chrome.tabs.query({
 		url: "*://misskey.xyz/*"
 	}, function (result) {
-		if (result.length !== 0) {
-			chrome.tabs.update(result[0].id, {}, function(tab) {
-				var code = "window.location.reload();";
-				for (var i = 0; i < tab.length; i++) chrome.tabs.executeScript(tab[i].id, {code: code});
+		for (var i = 0; i < result.length; i++) {
+			chrome.tabs.update(result[i].id, {}, function(tab) {
+				var cmd = "location.reload();";
+				chrome.tabs.executeScript(tab.id, {code: cmd});
 			});
-		};
+		}
 	});
+}
+
+function putLangs(lang) {
+	var elements = document.getElementsByClassName("translate");
+	for (var i = 0; i < elements.length; i++) {
+		for (var key in language[lang]) {
+			if (elements[i].innerText === "$" + key) {
+				elements[i].innerText = language[lang][key];
+			}
+		}
+	}
 }
 
 /*
