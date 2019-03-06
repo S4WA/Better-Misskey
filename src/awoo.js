a702500e4eafcbe9201c8df445dfafe76f039663
Drip <contact@s4wa.me>
2019-03-06 10:18:46 +0900

    Add files via upload

--- src/awoo.js
+++ src/awoo.js
@@ -1,9 +1,7 @@
-/*window.onload = function() {
-	console.log(
-		"option page\n - chrome-extension://bkmgmcphjfddgejhddnhjdbaebblecpe/options/options.html" + "\n" + 
-		"source code\n - https://github.com/S4WA/Better-Misskey"
-	);
-};*/
+console.log(
+	"option page\n - chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/options/options.html" + "\n" + 
+	"source code\n - https://github.com/S4WA/Better-Misskey"
+);
 
 /*chrome.tabs.create({
 	"url": chrome.extension.getURL("options/options.html"),
