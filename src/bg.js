9e4495c502e271d3242dc45e5a2689dcd7cc4332
S4WA <s4wa@outlook.com>
2019-04-16 12:27:51 +0900

    support da misskey.io

--- src/bg.js
+++ src/bg.js
@@ -6,17 +6,19 @@ chrome.storage.sync.get(null, function(items) {
 			"title": "Misskeyで共有する",
 			"onclick": function () {
 				chrome.tabs.getSelected(null, function(tab) {
-					var str = tab.title + " " + tab.url;
-					chrome.tabs.query({
-						url: "*://misskey.xyz/*"
-					}, function (result) {
-						if (result.length !== 0) {
-							var cmd = "var drafts = JSON.parse(localStorage.drafts); drafts.note.data.text = \"" + str + "\"; localStorage.drafts = JSON.stringify(drafts); location.reload();";
-							chrome.tabs.update(result[0].id, { active : true }, function(tab) {
-								chrome.tabs.executeScript(result[0].id, { code: cmd })
-							});
-						};
-					});
+					var str = tab.title + " " + tab.url, targets = ["*://misskey.xyz/*", "*://misskey.io/*"];
+					for (var i in targets) {
+						chrome.tabs.query({
+							url: targets[i]
+						}, function (result) {
+							if (result.length !== 0) {
+								var cmd = "var drafts = JSON.parse(localStorage.drafts); drafts.note.data.text = \"" + str + "\"; localStorage.drafts = JSON.stringify(drafts); location.reload();";
+								chrome.tabs.update(result[0].id, { active : true }, function(tab) {
+									chrome.tabs.executeScript(result[0].id, { code: cmd })
+								});
+							};
+						});
+					}
 				});
 			}
 		});
